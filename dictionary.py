eafa910a7fa7aa7e298084067c6d682367be6f88
sunil9889 <skk9889@gmail.com>
2020-05-11 18:15:22 +0530

    dictionary commit

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+dict ={}
+dict['one'] = 'This is one'
+dict[2] = 'This is two'
+tinydict = {'code':'red','dept':'IT'}
+
+print (dict['one'])
+print (dict[2])
+print (tinydict)
+print (tinydict.keys())
+print (tinydict.values())
+
+
